a1f5ca33d838d9787ace1397bc5d674398af9dd6
Josevillar013 <143235180+Josevillar013@users.noreply.github.com>
2025-12-14 00:30:27 +0000

    abcin complet

---
+++ abcin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Ingresa dos numeros: ";
+    cin >> a >> b;
+
+    cout << "La suma es: " << a + b << endl;
+    return 0;
+}
